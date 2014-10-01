8f1ab75fe7afa56981c09df83656f6290b712920
hasufell <hasufell@hasufell.de>
2014-10-01 03:03:09 +0200

    Set white background for the drawing area

--- Diagram.hs
+++ Diagram.hs
@@ -9,9 +9,9 @@ import Util
 -- |Create the Diagram from the VTable.
 diagFromVTable :: VTable -> Diagram Cairo R2
 diagFromVTable meshArr
-  = square 500 # lwG 0.05
-    `atop` position (zip (map mkPoint . filterValidCoords 0 500 $ meshArr)
+  = position (zip (map mkPoint . filterValidCoords 0 500 $ meshArr)
                      (repeat dot)) # moveTo (p2(-250, -250))
+   `atop` square 500 # lwG 0.05 # bg white
     where dot           = (circle 2 :: Diagram Cairo R2) # fc black
           mkPoint (x,y) = p2 (x,y)
 
