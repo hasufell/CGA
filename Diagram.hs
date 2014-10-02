c018304319ff9396227fcb8d0805e75aeffdf89c
hasufell <hasufell@hasufell.de>
2014-10-02 14:37:51 +0200

    Don't draw borders for the white square

--- Diagram.hs
+++ Diagram.hs
@@ -18,7 +18,7 @@ diagFromVTable :: DiagProp -> VTable -> Diagram Cairo R2
 diagFromVTable prop vt
   = position (zip (map mkPoint . filter (inRange 0 500) $ vt)
                   (repeat dot)) # moveTo (p2(-250, -250))
-     `atop` square 500 # lwG 0.05 # bg white
+     `atop` square 500 # lwG 0.00 # bg white
         where dot           = (circle $
                                getThickness prop :: Diagram Cairo R2) # fc black
               mkPoint (x,y) = p2 (x,y)
