da3f71bfc01b3d3dd63d186e16cb515076f9bc35
hasufell <hasufell@hasufell.de>
2014-10-08 16:27:59 +0200

    Add removeItem utility function

--- Util.hs
+++ Util.hs
@@ -11,3 +11,8 @@ splitBy f s =  case dropWhile f s of
                  [] -> []
                  s' -> w : splitBy f s''
                    where (w, s'') = break f s'
+
+
+-- |Remove a given item from a list.
+removeItem :: (Eq a) => a -> [a] -> [a]
+removeItem x = foldr (\x' y -> if x' == x then y else x':y) []
