f68d4c2ba7f75e384142ab4a3169e1869bfea76e
hasufell <hasufell@hasufell.de>
2014-12-17 05:11:18 +0100

    TESTS: initial implementation of a few test cases

---
+++ Test/Vector.hs
@@ -0,0 +1,82 @@
+{-# OPTIONS_HADDOCK ignore-exports #-}
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
+
+module Test.Vector where
+
+import Algebra.Vector
+import Control.Applicative
+{- import Control.Monad -}
+import Diagrams.TwoD.Types
+import Test.QuickCheck
+
+
+instance Arbitrary R2 where
+  arbitrary = curry r2 <$> arbitrary <*> arbitrary
+
+
+instance Arbitrary P2 where
+  arbitrary = curry p2 <$> arbitrary <*> arbitrary
+
+
+inRangeProp1 :: Square -> Bool
+inRangeProp1 sq@((x1, y1), _) =
+  inRange sq (p2 (x1, y1))
+
+
+inRangeProp2 :: Square -> Bool
+inRangeProp2 sq@(_, (x2, y2)) =
+  inRange sq (p2 (x2, y2))
+
+
+inRangeProp3 :: Square -> Bool
+inRangeProp3 sq@((x1, _), (_, y2)) =
+  inRange sq (p2 (x1, y2))
+
+
+inRangeProp4 :: Square -> Bool
+inRangeProp4 sq@((_, y1), (x2, _)) =
+  inRange sq (p2 (x2, y1))
+
+
+inRangeProp5 :: Square -> Bool
+inRangeProp5 sq@((x1, y1), (x2, y2)) =
+  inRange sq (p2 (x1 + ((x2 - x1) / 2), y1 + ((y2 - y1) / 2)))
+
+
+onPTProp1 :: PT -> Bool
+onPTProp1 pt = onPT id pt == pt
+
+
+onPTProp2 :: PT -> Positive R2 -> Bool
+onPTProp2 pt (Positive (R2 rx ry))
+  = onPT (\(x, y) -> (x + rx, y + ry)) pt /= pt
+
+
+getAngleProp1 :: Positive Vec -> Positive Vec -> Bool
+getAngleProp1 (Positive (R2 x1 _)) (Positive (R2 x2 _))
+  = getAngle (R2 x1 0) (R2 x2 0) == 0
+
+
+getAngleProp2 :: Positive Vec -> Positive Vec -> Bool
+getAngleProp2 (Positive (R2 _ y1)) (Positive (R2 _ y2))
+  = getAngle (R2 0 y1) (R2 0 y2) == 0
+
+
+getAngleProp3 :: Positive Vec -> Positive Vec -> Bool
+getAngleProp3 (Positive (R2 x1 _)) (Positive (R2 x2 _))
+  = getAngle (R2 (negate x1) 0) (R2 x2 0) == pi
+
+
+getAngleProp4 :: Positive Vec -> Positive Vec -> Bool
+getAngleProp4 (Positive (R2 _ y1)) (Positive (R2 _ y2))
+  = getAngle (R2 0 (negate y1)) (R2 0 y2) == pi
+
+
+getAngleProp5 :: Positive Vec -> Positive Vec -> Bool
+getAngleProp5 (Positive (R2 x1 _)) (Positive (R2 _ y2))
+  = getAngle (R2 x1 0) (R2 0 y2) == pi / 2
+
+
+getAngleProp6 :: Positive Vec -> Positive Vec -> Bool
+getAngleProp6 (Positive (R2 _ y1)) (Positive (R2 x2 _))
+  = getAngle (R2 x2 0) (R2 0 y1) == pi / 2
---
+++ TestMain.hs
@@ -0,0 +1,29 @@
+{-# OPTIONS_HADDOCK ignore-exports #-}
+
+
+import Test.QuickCheck
+import Test.Vector
+
+
+deepCheck :: Testable prop => prop -> IO ()
+deepCheck = quickCheckWith (stdArgs { maxSuccess = 1000})
+
+
+main :: IO ()
+main = do
+  putStrLn "testing inRange:"
+  deepCheck inRangeProp1
+  deepCheck inRangeProp2
+  deepCheck inRangeProp3
+  deepCheck inRangeProp4
+  deepCheck inRangeProp5
+  putStrLn "testing onPT:"
+  deepCheck onPTProp1
+  deepCheck onPTProp2
+  putStrLn "testing getAngle:"
+  deepCheck getAngleProp1
+  deepCheck getAngleProp2
+  deepCheck getAngleProp3
+  deepCheck getAngleProp4
+  deepCheck getAngleProp5
+  deepCheck getAngleProp6
